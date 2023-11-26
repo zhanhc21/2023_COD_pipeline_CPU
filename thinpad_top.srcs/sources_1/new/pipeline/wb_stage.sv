db27aa3c9ac7504bba31d8188c1e6355b3b5b866
李想 <2021010763@secoder.net>
2023-11-26 13:42:31 +0800

    feat: add WB stage

---
+++ thinpad_top.srcs/sources_1/new/pipeline/wb_stage.sv
@@ -0,0 +1,28 @@
+module WB_Stage (
+    input wire clk_i,
+    input wire rst_i,
+
+    // signals from MEM stage
+    input wire [31:0] wb_pc_i,
+    input wire [31:0] wb_instr_i,
+    input wire [31:0] wb_rf_wdata_i,
+    input wire [ 4:0] wb_rf_waddr_i,
+    input wire        wb_rf_wen_i,
+
+    // stall signal and flush signal
+    input wire stall_i,
+    input wire flush_i,
+
+    // signals to regfile
+    output reg [31:0] rf_wdata_o,
+    output reg [ 4:0] rf_waddr_o,
+    output reg        rf_wen_o,
+);
+    // TODO: stall signal and flush signal
+    always_comb begin
+        rf_wen_o = wb_rf_wen_i;
+        rf_waddr_o = wb_rf_waddr_i;
+        rf_wdata_o = wb_rf_wdata_i;
+    end
+
+endmodule
