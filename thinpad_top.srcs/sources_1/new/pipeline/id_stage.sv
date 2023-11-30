b6c0a9fdb1da5fd0818334b0e50b642dcf8b0a3c
hanyang-21 <1935460354@qq.com>
2023-11-30 17:32:33 +0800

    fix: forward unit & load hazard

--- thinpad_top.srcs/sources_1/new/pipeline/id_stage.sv
+++ thinpad_top.srcs/sources_1/new/pipeline/id_stage.sv
@@ -15,6 +15,9 @@ module ID_Stage (
     input wire [31:0] rf_rdata_b_i,
     output reg [ 4:0] rf_raddr_a_o,
     output reg [ 4:0] rf_raddr_b_o,
+    
+    // signal to controller
+    output reg exe_first_time_o,
 
     // signals to EXE stage
     output reg [31:0] exe_pc_o,
@@ -198,6 +201,7 @@ module ID_Stage (
                 rf_wen_reg = 1;
                 mem_en_reg = 0;
                 mem_wen_reg = 0;
+                rs2_reg = 5'b0;
             end
             OPCODE_LB_LW: begin
                 inst_type_reg = TYPE_I;
@@ -207,6 +211,7 @@ module ID_Stage (
                 rf_wen_reg = 1;
                 mem_en_reg = 1;
                 mem_wen_reg = 0;
+                rs2_reg = 5'b0;
             end
             OPCODE_LUI: begin
                 inst_type_reg = TYPE_U;
@@ -285,6 +290,10 @@ module ID_Stage (
             exe_rf_waddr_o <= 5'b0;
             exe_rf_wen_o <= 1'b0;
         end else begin
+            if (exe_pc_o != pc_reg)
+                exe_first_time_o <= 1'b1;
+            else 
+                exe_first_time_o <= 1'b0;
             exe_pc_o <= pc_reg;
             exe_instr_o <= inst_reg;
             exe_rf_raddr_a_o <= rs1_reg;
--- thinpad_top.srcs/sources_1/new/pipeline/pipeline_controller.sv
+++ thinpad_top.srcs/sources_1/new/pipeline/pipeline_controller.sv
@@ -3,26 +3,28 @@ module pipeline_controller(
     input wire rst_i,
 
     // signals from ID stage
-    input wire [ 4:0] id_rf_raddr_a_i,
-    input wire [ 4:0] id_rf_raddr_b_i,
+    input wire [4:0] id_rf_raddr_a_i,
+    input wire [4:0] id_rf_raddr_b_i,
 
     // signals from ID/EXE pipeline registers
-    input wire [ 4:0] exe_rf_raddr_a_i,
-    input wire [ 4:0] exe_rf_raddr_b_i,
+    input wire [31:0] exe_pc_i,
+    input wire [4:0] exe_rf_raddr_a_i,
+    input wire [4:0] exe_rf_raddr_b_i,
     input wire        exe_mem_en_i,
     input wire        exe_mem_wen_i,
-    input wire [ 4:0] exe_rf_waddr_i,
+    input wire [4:0] exe_rf_waddr_i,
+    input wire exe_first_time_i,
 
     // signals from EXE/MEM pipeline registers
     input wire [31:0] mem_rf_wdata_i,
-    input wire [ 4:0] mem_rf_waddr_i,
+    input wire [4:0] mem_rf_waddr_i,
     input wire        mem_rf_wen_i,
     input wire        mem_mem_en_i,
     input wire        mem_mem_wen_i,
 
     // signals from MEM/WB pipeline registers
     input wire [31:0] wb_rf_wdata_i,
-    input wire [ 4:0] wb_rf_waddr_i,
+    input wire [4:0] wb_rf_waddr_i,
     input wire        wb_rf_wen_i,
     input wire exe_if_pc_mux_i,
 
@@ -50,6 +52,7 @@ module pipeline_controller(
 );
 
     logic m_busy;  // if memory busy (IF/MEM)
+    reg [31:0] exe_pc_i_reg;
 
     assign m_busy = if_busy_i | mem_busy_i;
 
@@ -75,6 +78,8 @@ module pipeline_controller(
         end else if (exe_mem_en_i && !exe_mem_wen_i &&  // load hazard
             (exe_rf_waddr_i == id_rf_raddr_a_i || exe_rf_waddr_i == id_rf_raddr_b_i)) begin
             if_stall_o = 1'b1;
+            id_stall_o = 1'b1;
+            exe_stall_o = 1'b1;
         end
     end
 
@@ -86,27 +91,29 @@ module pipeline_controller(
         exe_forward_alu_b_mux_o = 1'b0;
 
         // ALU@EXE/MEM->ALU
-        if (mem_rf_wen_i) begin
-            if (mem_rf_waddr_i == exe_rf_raddr_a_i) begin
-                exe_forward_alu_a_o = mem_rf_wdata_i;
-                exe_forward_alu_a_mux_o = 1'b1;
+        if (exe_first_time_i) begin
+            if (mem_rf_wen_i) begin
+                if (mem_rf_waddr_i == exe_rf_raddr_a_i && mem_rf_waddr_i != 0) begin
+                    exe_forward_alu_a_o = mem_rf_wdata_i;
+                    exe_forward_alu_a_mux_o = 1'b1;
+                end
+                if (mem_rf_waddr_i == exe_rf_raddr_b_i && mem_rf_waddr_i != 0) begin
+                    exe_forward_alu_b_o = mem_rf_wdata_i;
+                    exe_forward_alu_b_mux_o = 1'b1;
+                end
             end
-            if (mem_rf_waddr_i == exe_rf_raddr_b_i) begin
-                exe_forward_alu_b_o = mem_rf_wdata_i;
-                exe_forward_alu_b_mux_o = 1'b1;
+            // ALU@MEM/WB->ALU
+            if (wb_rf_wen_i) begin
+                if(wb_rf_waddr_i == exe_rf_raddr_a_i) begin
+                    exe_forward_alu_a_o = wb_rf_wdata_i;
+                    exe_forward_alu_a_mux_o = 1'b1;
+                end
+                if(wb_rf_waddr_i == exe_rf_raddr_b_i) begin
+                    exe_forward_alu_b_o = wb_rf_wdata_i;
+                    exe_forward_alu_b_mux_o = 1'b1;
+                end
             end
-        end
-        // ALU@MEM/WB->ALU
-        if (wb_rf_wen_i) begin
-            if(wb_rf_waddr_i == exe_rf_raddr_a_i) begin
-                exe_forward_alu_a_o = wb_rf_wdata_i;
-                exe_forward_alu_a_mux_o = 1'b1;
-            end
-            if(wb_rf_waddr_i == exe_rf_raddr_b_i) begin
-                exe_forward_alu_b_o = wb_rf_wdata_i;
-                exe_forward_alu_b_mux_o = 1'b1;
-            end
-        end   
+        end  
     end
     
 
