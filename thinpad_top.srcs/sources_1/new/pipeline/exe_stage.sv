54bcbbe0bb1401caf8f84fc7565ceda59cde8040
zhanhc21 <zhanhc21@mails.tsinghua.edu.cn>
2023-11-30 14:32:18 +0800

    feat: id/exe: andn, sbset

--- thinpad_top.srcs/sources_1/new/pipeline/exe_stage.sv
+++ thinpad_top.srcs/sources_1/new/pipeline/exe_stage.sv
@@ -33,7 +33,7 @@ module EXE_Stage (
     output reg         mem_mem_en_o,      // if use DM
     output reg         mem_mem_wen_o,     // if write DM (0: read DM, 1: write DM)
     output reg  [ 4:0] mem_rf_waddr_o,    // rf addr
-    output reg         mem_rf_wen_o,       // if write back (rf)
+    output reg         mem_rf_wen_o,      // if write back (rf)
 
     // signals to ALU
     input  wire [31:0] alu_result_i,
@@ -64,7 +64,10 @@ module EXE_Stage (
         SRLI  = 16,
         SW    = 17,
         XOR   = 18,
-        NOP   = 19
+        ANDN  = 19,
+        SBSET = 20,
+        MINU  = 21,
+        NOP   = 22
     } op_type;
     op_type instr_type;
 
@@ -76,8 +79,14 @@ module EXE_Stage (
             7'b0110011: begin
                 if (exe_instr_i[14:12] == 3'b000)
                     instr_type = ADD;
-                else if (exe_instr_i[14:12] == 3'b111)
-                    instr_type = AND;
+                else if (exe_instr_i[14:12] == 3'b111) begin
+                    if (exe_instr_i[31:25] == 7'b0)
+                        instr_type = AND;
+                    else if exe_instr_i[31:25] == 7'b0100000
+                        instr_type = ANDN;
+                    else // exe_instr_i[31:25] == 7'b0010100
+                        instr_type = SBSET;
+                end
                 else if (exe_instr_i[14:12] == 3'b110)
                     instr_type = OR;
                 else // (exe_instr_i[14:12] == 3'b100)
@@ -125,12 +134,16 @@ module EXE_Stage (
         // if (stall_i == 1'b0) begin
         alu_op_o = exe_alu_op_i;
         if (exe_alu_a_mux_i == 1'b0)
+            // rs1
             alu_operand_a_o = exe_rf_rdata_a_i;
         else
+            // pc
             alu_operand_a_o = exe_pc_i;
         if (exe_alu_b_mux_i == 1'b0)
+            // imm
             alu_operand_b_o = exe_imm_i;
         else 
+            // rs2
             alu_operand_b_o = exe_rf_rdata_b_i;
         // end else begin
         //     alu_op_o = 4'd1;
@@ -168,17 +181,14 @@ module EXE_Stage (
                         end
                     end
                     BNE: begin
-                        if (alu_result_i != 0) begin
+                        if (exe_rf_rdata_a_i != exe_rf_rdata_b_i) begin
                             if_pc_mux_o <= 1'b1;
-                            if_pc_o <= exe_pc_i + (exe_imm_i << 1) | SignExt;    
+                            if_pc_o <= alu_result_i;
+                            // if_pc_o <= exe_pc_i + (exe_imm_i << 1) | SignExt;    
                         end else begin
                             if_pc_mux_o <= 1'b0;
                             if_pc_o <= exe_pc_i;        
                         end
-                    end
-                    LB: begin
-                        if_pc_mux_o <= 1'b0;
-                        if_pc_o <= exe_pc_i;               
                     end
                     SB: begin
                         if_pc_mux_o <= 1'b0;
@@ -197,6 +207,18 @@ module EXE_Stage (
                         if_pc_o <= exe_pc_i;
                         mem_alu_result_o <= exe_imm_i;
                     end
+                    JAL: begin
+                        if_pc_mux_o <= 1'b1;
+                        // pc += offset
+                        if_pc_o <= alu_result_i;
+                        mem_alu_result_o <= exe_pc_i + 32'd4;
+                    end
+                    JALR: begin
+                        if_pc_mux_o <= 1'b1;
+                        // pc = rs1 + offset
+                        if_pc_o <= (exe_imm_i + exe_rf_rdata_a_i) & ~1;
+                        mem_alu_result_o <= exe_pc_i + 32'd4;
+                    end
                     NOP: begin
                         if_pc_mux_o <= 1'b0;
                         if_pc_o <= exe_pc_i;
@@ -204,6 +226,7 @@ module EXE_Stage (
                         mem_mem_wen_o <= 1'b0;
                         mem_rf_wen_o <= 1'b0;
                     end
+                    // add(i),and(i),or(i),auipc,lb,lw,xor,slli,srli,andn,sbset
                     default: begin
                         if_pc_mux_o <= 1'b0;
                         if_pc_o <= exe_pc_i;                  
